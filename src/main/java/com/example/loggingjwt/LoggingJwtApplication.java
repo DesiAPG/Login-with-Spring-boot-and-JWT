dc91b8b109306a845aecf181506b48133ad4c608
Desireeee <74121435+DesiAPG@users.noreply.github.com>
2023-07-28 07:40:47 -0500

    Initial commit

---
+++ src/main/java/com/example/loggingjwt/LoggingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.loggingjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loggingjwt/LoggingJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggingjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
